889e24172fa194934beb19d89347c8e8b2b2430f
Daria <daria.vedernikowa@gmail.com>
2022-06-05 19:41:49 +0200

    without dataclass in button

--- corgiinvasion/button.py
+++ corgiinvasion/button.py
@@ -3,22 +3,21 @@ from dataclasses import dataclass, InitVar, field
 from typing import Any
 
 
-@dataclass
 class Button:
-    screen: Any
-    message: str
-    position: Any = field(default_factory=tuple)
-    width: int = 200
-    height: int = 50
-    button_color: tuple = (0, 255, 0)
-    text_color: tuple = (255, 255, 255)
-    pygame.font.init()
-    font: Any = pygame.font.SysFont(None, 48)
-    rect: Any = pygame.Rect(0, 0, width, height)
-    #rect.center = position
-
-    def __post_init__(self):
+    def __init__(self, screen, message,position):
+        self.screen = screen
+        self.message = message
+        self.position = position
+        self.width = 200
+        self.height = 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.position
+        self.image()
+
+    def image(self):
         self.msg_image = self.font.render(self.message, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
